4ef2086ad1211cf2ca900e8fa57ff336d29bfb98
KevenPotter <keven_potter@163.com>
2020-01-01 00:26:38 +0800

    Commit

--- src/main/java/com/kevenpotter/student/controller/IndexDashBoardController.java
+++ src/main/java/com/kevenpotter/student/controller/IndexDashBoardController.java
@@ -2,6 +2,8 @@ package com.kevenpotter.student.controller;
 
 import com.kevenpotter.student.config.WebSocketConfig;
 import com.kevenpotter.student.service.IndexService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,12 +24,13 @@ import java.util.concurrent.CopyOnWriteArraySet;
 @RestController
 public class IndexDashBoardController {
 
+    /*定义日志记录器，用来记录必要信息*/
+    private static Logger logger = LoggerFactory.getLogger(IndexDashBoardController.class);
+
     /*静态变量,用来记录当前在线连接数.应该把它设计成线程安全的.*/
     private static int onlineCount = 0;
-    /*concurrent包的线程安全Set,用来存放每个客户端对应的MyWebSocket对象.若要实现服务端与单一客户端通信的话,可以使用Map来存放,其中Key可以为用户标识.*/
-    private static CopyOnWriteArraySet<IndexDashBoardController> webSocketSet = new CopyOnWriteArraySet<IndexDashBoardController>();
-    /*与某个客户端的连接会话,需要通过它来给客户端发送数据.*/
-    private Session session;
+    /*与某个客户端的连接会话列表,需要通过它来给客户端广播数据.*/
+    private static CopyOnWriteArraySet<Session> sessionSet = new CopyOnWriteArraySet<Session>();
 
     private static IndexService indexService;
 
@@ -44,21 +47,22 @@ public class IndexDashBoardController {
      */
     @OnOpen
     public void onOpen(Session session) {
-        this.session = session;
-        System.out.println("sessionId: " + session.getId());
-        webSocketSet.add(this);
         IndexDashBoardController.addOnlineCount();
-        try {
-            session.getBasicRemote().sendText(String.valueOf(IndexDashBoardController.getOnlineCount()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        sessionSet.add(session);
+        IndexDashBoardController.broadcast();
     }
 
+    /**
+     * @param session session为与某个客户端的连接会话,需要通过它来给客户端发送数据
+     * @author KevenPotter
+     * @date 2019-12-24 10:22:01
+     * @description 连接关闭调用的方法
+     */
     @OnClose
-    public void onClose() {
-        webSocketSet.remove(this);
+    public void onClose(Session session) {
+        sessionSet.remove(session);
         IndexDashBoardController.subOnlineCount();
+        IndexDashBoardController.broadcast();
     }
 
     /**
@@ -87,5 +91,21 @@ public class IndexDashBoardController {
      */
     private static synchronized void subOnlineCount() {
         IndexDashBoardController.onlineCount--;
+    }
+
+    /**
+     * @author KevenPotter
+     * @date 2020-01-01 00:00:01
+     * @description 对信息进行广播, 也就是更新首页的visits(在线浏览)信息
+     */
+    private static void broadcast() {
+        try {
+            for (Session session : sessionSet) {
+                session.getBasicRemote().sendText(String.valueOf(IndexDashBoardController.getOnlineCount()));
+            }
+        } catch (IOException e) {
+            logger.error("在线浏览广播发生错误. 请检查 [IndexDashBoardController.java] 代码.");
+            e.printStackTrace();
+        }
     }
 }
